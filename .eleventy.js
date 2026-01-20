a8619134770fb0e19711b200a2d5378d575df26e
Jesse Mortenson <jessemortenson@gmail.com>
2026-01-20 11:51:15 -0600

    Add robots.txt file

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/CNAME");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
   
   // Define episodes collection
   eleventyConfig.addCollection("episodes", function(collection) {
